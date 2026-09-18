4f7cce3ddebe57ad611e6d114d3c59ab4b4cedfa
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-18 23:23:54 +0100

    testability(practice): thread completion store through router

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -3,11 +3,15 @@ import 'package:go_router/go_router.dart';
 
 import '../dashboard/dashboard_v3_screen.dart';
 import '../loop/loop_flow_screen.dart';
+import '../practice/practice_completion_store.dart';
 import '../practice/practice_screen.dart';
 import '../shell/app_shell.dart';
 import '../shell/shell_placeholder_screen.dart';
 
-GoRouter createTrueGroundRouter() {
+GoRouter createTrueGroundRouter({
+  PracticeCompletionStore? practiceCompletionStore,
+  DateTime Function()? practiceNow,
+}) {
   return GoRouter(
     initialLocation: '/',
     routes: <RouteBase>[
@@ -39,7 +43,10 @@ GoRouter createTrueGroundRouter() {
               GoRoute(
                 path: '/practice',
                 name: 'practice',
-                builder: (context, state) => const PracticeScreen(),
+                builder: (context, state) => PracticeScreen(
+                  completionStore: practiceCompletionStore,
+                  now: practiceNow,
+                ),
               ),
             ],
           ),
