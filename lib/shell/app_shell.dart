5fe1015fc5f83ba4954b3652d9cb30e2cba7accc
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-25 01:00:35 +0100

    feat(i18n): show FR EN toggle in app shell header

--- lib/shell/app_shell.dart
+++ lib/shell/app_shell.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../design/app_theme.dart';
+import '../localization/language_toggle.dart';
 import '../localization/trueground_locale.dart';
 
 class AppShell extends StatelessWidget {
@@ -36,15 +37,18 @@ class AppShell extends StatelessWidget {
             ? Colors.transparent
             : TrueGroundColors.background,
         body: SafeArea(
-          child: onHome
-              ? Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    const _HomeGlassBackdrop(),
-                    navigationShell,
-                  ],
-                )
-              : navigationShell,
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              if (onHome) const _HomeGlassBackdrop(),
+              navigationShell,
+              const Positioned(
+                top: 8,
+                right: 12,
+                child: LanguageToggle(),
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: SafeArea(
           top: false,
